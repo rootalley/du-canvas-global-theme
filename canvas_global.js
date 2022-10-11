4cbbb33f81cd9b410a56a2cd5ecf5c249a1020a0
Endres <sendres@dom.edu>
2022-10-11 13:10:25 -0500

    Remove duplicate DUParseCourseID function

--- canvas_global.js
+++ canvas_global.js
@@ -15,11 +15,6 @@ function DUParseCourseID() {
     const truncator = new RegExp(location.hostname + '\/courses\/[1-9][0-9]*');
     const path = truncator.exec(location.href);
     return (path ? path[0].substring((location.hostname + '/courses/').length) : null);
-}function DUParseCourseID() {
-  'use strict';
-  const truncator = new RegExp(location.hostname + '\/courses\/[1-9][0-9]*');
-  const path = truncator.exec(location.href);
-  return (path ? path[0].substring((location.hostname + '/courses/').length) : null);
 }
 
 
@@ -27,17 +22,17 @@ function DUParseCourseID() {
  *  Append a CSS file to an HTML document head, thereby applying its styling to
  *  that web page.
  */
- function DUAppendCssFileToDocumentHead(cssFile, documentHead) {
-  'use strict';
-  try {
-    var linkElement = document.createElement('link');
-    linkElement.setAttribute('rel', 'stylesheet');
-    linkElement.setAttribute('type', 'text/css');
-    linkElement.setAttribute('href', cssFile);
-    documentHead.appendChild(linkElement);
-  } catch (e) {
-    console.log('DU Theme (info): style.css not loaded');
-  }
+function DUAppendCssFileToDocumentHead(cssFile, documentHead) {
+    'use strict';
+    try {
+        var linkElement = document.createElement('link');
+        linkElement.setAttribute('rel', 'stylesheet');
+        linkElement.setAttribute('type', 'text/css');
+        linkElement.setAttribute('href', cssFile);
+        documentHead.appendChild(linkElement);
+    } catch (e) {
+        console.log('DU Theme (info): style.css not loaded');
+    }
 }
 
 
@@ -48,24 +43,24 @@ function DUParseCourseID() {
  */
 var coursenum = DUParseCourseID();
 if (coursenum) {
-  /* add the CSS file to the Canvas page */
-  var cssPath = '/courses/' + coursenum + '/file_contents/course%20files/DU%20Theme/style.css';
-  var head = document.getElementsByTagName('HEAD')[0];
-  DUAppendCssFileToDocumentHead(cssPath, head);
+    /* add the CSS file to the Canvas page */
+    var cssPath = '/courses/' + coursenum + '/file_contents/course%20files/DU%20Theme/style.css';
+    var head = document.getElementsByTagName('HEAD')[0];
+    DUAppendCssFileToDocumentHead(cssPath, head);
 
-  /* if the page has a Rich Content Editor iframe, add the CSS there as well */
-  document.onreadystatechange = function () {
-    'use strict';
-    if (document.readyState != 'loading') {
-      var iframeList = document.getElementsByTagName("iframe");
-      for (let iframeItem of iframeList) {
-        if (iframeItem.id.slice(-4) === '_ifr') {
-          head = iframeItem.contentDocument.head;
-          DUAppendCssFileToDocumentHead(cssPath, head);
+    /* if the page has a Rich Content Editor iframe, add the CSS there as well */
+    document.onreadystatechange = function () {
+        'use strict';
+        if (document.readyState != 'loading') {
+            var iframeList = document.getElementsByTagName("iframe");
+            for (let iframeItem of iframeList) {
+                if (iframeItem.id.slice(-4) === '_ifr') {
+                    head = iframeItem.contentDocument.head;
+                    DUAppendCssFileToDocumentHead(cssPath, head);
+                }
+            }
         }
-      }
     }
-  }
 }
 
 
